281b85fc36c56df7f7a6ffab899ffe6510db14ab
petar-savov <p.savov@outlook.com>
2023-12-12 21:01:47 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Ray tracing based on [the series of books](https://github.com/RayTracing/raytracing.github.io).
+
+To run
+
+> clang++ -std=c++20 -o ./bin/raytracer main.cc
+
+> ./bin/raytracer > output/output.ppm
---
+++ main.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int image_width = 256;
+    int image_height = 256;
+
+    cout << "P3\n"
+         << image_width << ' ' << image_height << "\n255\n";
+
+    for (int j = image_height - 1; j >= 0; --j)
+    {
+        for (int i = 0; i < image_width; ++i)
+        {
+            auto r = double(i) / (image_width - 1);
+            auto g = double(j) / (image_height - 1);
+            auto b = 0.98;
+
+            int ir = static_cast<int>(255.999 * r);
+            int ig = static_cast<int>(255.999 * g);
+            int ib = static_cast<int>(255.999 * b);
+
+            cout << ir << ' ' << ig << ' ' << ib << '\n';
+        }
+    }
+    return 0;
+}
