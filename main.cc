1470e13a3efeb57f6ade661f2aa66b640eead69e
petar-savov <p.savov@outlook.com>
2023-12-19 14:42:14 +0000

    Vec3 implementation

--- main.cc
+++ main.cc
@@ -1,29 +1,27 @@
+#include "color.h"
+#include "vec3.h"
+
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
+
     int image_width = 256;
     int image_height = 256;
 
-    cout << "P3\n"
-         << image_width << ' ' << image_height << "\n255\n";
+    std::cout << "P3\n"
+              << image_width << ' ' << image_height << "\n255\n";
 
-    for (int j = image_height - 1; j >= 0; --j)
+    for (int j = 0; j < image_height; ++j)
     {
         for (int i = 0; i < image_width; ++i)
         {
-            auto r = double(i) / (image_width - 1);
-            auto g = double(j) / (image_height - 1);
-            auto b = 0.98;
-
-            int ir = static_cast<int>(255.999 * r);
-            int ig = static_cast<int>(255.999 * g);
-            int ib = static_cast<int>(255.999 * b);
-
-            cout << ir << ' ' << ig << ' ' << ib << '\n';
+            auto pixel_color = color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0);
+            write_color(std::cout, pixel_color);
         }
     }
-    return 0;
+
+    std::clog << "\rDone.\n";
 }
